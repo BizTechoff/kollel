a9b9cad28533fe2b8bbab65df6312b615c1fcfb9
Motidru <motidru@gmail.com>
2023-06-16 11:02:14 +0300

    v2023.06.16

--- src/app/terms.ts
+++ src/app/terms.ts
@@ -1,7 +1,7 @@
 import './common/dateFunc'
 
 export const terms = {
-    appVersion: '2023.06.13',
+    appVersion: '2023.06.16',
     username: "שם",
     signIn: "Sign In",
     confirmPassword: "Confirm Password",
